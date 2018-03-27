1f14e8ed059a64a1cc378a8bb98a4382648a5966
hunyong.cho <hunyong.cho@gmail.com>
2018-03-27 11:56:52 -0400

    imputation by a single step

---
+++ C0102-imputation-SingleStep.R
@@ -0,0 +1,11 @@
+source("F0101-variableSet.R")
+
+# list of time points and variable categories
+  .timePoints
+  .categories
+
+# variable subsetting
+  tmp <- varSet(category="comorbidity", time = "all", covariate = c("base", "demographic", "IBSS"))
+  tmp <- varSet(category="anger", time = c("bl", "fu"), covariate = c("base", "demographic", "IBSS"))
+
+mice(data.working[,tmp$variable], m=20)
